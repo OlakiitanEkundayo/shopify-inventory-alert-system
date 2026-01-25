7cb30ec84f45e2c8938d37d71ca87308483d4f93
Olakiitan <ekundayoopeyemi123@gmail.com>
2026-01-25 22:35:38 +0100

    Add Shopify service configuration

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         ],
     ],
 
+    'shopify' => [
+        'shop_domain' => env('SHOPIFY_APP_URL'),
+        'access_token' => env('SHOPIFY_ACCESS_TOKEN'),
+        'api_version' => env('SHOPIFY_API_VERSION', '2024-01')
+    ],
+
 ];
